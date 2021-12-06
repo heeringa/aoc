10920a81126a03ca9a616e267de17a0383943d19
Brent Heeringa <heeringa@dropbox.com>
2021-12-06 08:01:06 -0500

    day 6 -- easiest day so far

---
+++ 2021/6/fish.py
@@ -0,0 +1,24 @@
+import sys
+
+def nextday(counts):
+    newcounts = [0]*9
+    for i, count in enumerate(counts):
+        print(i,newcounts)
+        if i == 0:
+            newcounts[8] = count
+            newcounts[6] = count
+        else:
+            newcounts[i-1] = newcounts[i-1] + count
+    return newcounts
+
+if __name__ == '__main__':
+    counts = [0]*9
+    for fish in sys.stdin.readline().split(','):
+        fish = int(fish)
+        counts[fish] = counts[fish] + 1
+    
+    days = 256
+    for _ in range(days):
+        counts = nextday(counts)
+    print(sum(counts))
+        
